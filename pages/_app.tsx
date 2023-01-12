bcb90ed5e8a1f22a1f7683b323fd0413bfd4e9c8
aosdinkim <master@awesomble.com>
2023-01-12 13:23:11 +0900

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import { Amplify } from 'aws-amplify'
 import awsExports from '../src/aws-exports'
 
-Amplify.configure({ ...awsExports, ssr: true, userPoolId: 'ap-northeast-2_ntUSlOELB' })
+Amplify.configure({ ...awsExports, ssr: true, userPoolId: 'ap-northeast-2_ntUSlOELB', UserPoolArn: 'arn:aws:cognito-idp:ap-northeast-2:199657251396:userpool/ap-northeast-2_MSa7zTSaD' })
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
