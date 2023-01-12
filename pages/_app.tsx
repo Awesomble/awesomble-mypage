124f006167ce6bd07ca308df286db0dd52374ad1
aosdinkim <master@awesomble.com>
2023-01-12 12:52:29 +0900

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import { Amplify } from 'aws-amplify'
 import awsExports from '../src/aws-exports'
 
-Amplify.configure({ ...awsExports, ssr: true, aws_user_pools_id: 'awesomble' })
+Amplify.configure({ ...awsExports, ssr: true, userPoolId: 'ap-northeast-2_ntUSlOELB' })
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
